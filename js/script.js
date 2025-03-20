9aaafbf39462bf43ad0fc8e39aae62c0c2bdd6da
Clara Ricioni <bergamoclara9@gmail.com>
2025-03-19 22:07:01 -0300

    closes #6 - feat: API get json

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     function limpa_formulário_cep() {
         $("#inputAddress").val("");
@@ -6,23 +6,38 @@ $(document).ready(function () {
         $("#inputCity").val("");
         $("#inputState").val("");
     }
-    $("#inputCep").blur(function () {
+    $("#inputCep").blur(function() {
         var cep = $(this).val().replace(/\D/g, '');
 
         if (cep != "") {
             var validacep = /^[0-9]{8}$/;
 
-            if (validacep.test(cep)) {
+            if(validacep.test(cep)) {
                 $("#inputAddress").val("...");
                 $("#inputDistrict").val("...");
                 $("#inputCity").val("...");
                 $("#inputState").val("...");
-            }
+
+                $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+                    if (!("erro" in dados)) {
+                        $("#inputAddress").val(dados.logradouro);
+                        $("#inputDistrict").val(dados.bairro);
+                        $("#inputCity").val(dados.localidade);
+                        $("#inputState").val(dados.uf);
+
+                        $("#inputNumber").prop("disabled", false).attr("required", true);
+                    } 
+                    else {
+                        limpa_formulário_cep();
+                        alert("CEP não encontrado.");
+                    }
+                });
+            } 
             else {
                 limpa_formulário_cep();
                 alert("Formato de CEP inválido.");
             }
-        }
+        } 
         else {
             limpa_formulário_cep();
         }
